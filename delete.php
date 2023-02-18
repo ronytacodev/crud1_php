5f7ecf3d3cc2c078514f44a376cec13c535ac947
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-02-18 13:08:35 -0500

    it was previously builded the Read funcionality and now we'll build the Delete funcionality of our CRUD application

---
+++ delete.php
@@ -0,0 +1,12 @@
+<?php
+include("conexion.php");
+$con = conectar();
+
+$cod_estudiante = $_GET['id'];
+
+$sql = "DELETE FROM alumnos WHERE cod_estudiante = '$cod_estudiante' ";
+$query = mysqli_query($con, $sql);
+
+if ($query) {
+    Header("Location: alumno.php");
+}
